c93204ba7c0a1f6ede8ce11025899a718486157d
truemichaelwolf@gmail.com <truemichaelwolf@gmail.com>
2025-01-16 16:14:21 +0800

    Updates

---
+++ manifest.js
@@ -0,0 +1,8 @@
+{
+    "short_name": "The Way of Reading",
+    "name": "The Way of Reading",
+    "start_url": ".",
+    "display": "standalone",
+    "theme_color": "#000000",
+    "background_color": "#ffffff"
+  }
---
+++ precache-manifest.7b126e38df0d1d4ae83c6517b1bf4001.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "879dd5ab2edf6919d9fdff3c9fd55f30",
+    "url": "/TheWayofReading/index.html"
+  },
+  {
+    "revision": "0ced499cb6c3d794f068",
+    "url": "/TheWayofReading/static/js/2.e230c356.chunk.js"
+  },
+  {
+    "revision": "d641aa03037d5e9248f1",
+    "url": "/TheWayofReading/static/js/main.0e26da73.chunk.js"
+  },
+  {
+    "revision": "410e09673771d7c7fd46",
+    "url": "/TheWayofReading/static/js/runtime~main.6d602d51.js"
+  }
+]);
